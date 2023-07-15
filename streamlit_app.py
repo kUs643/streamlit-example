0bb550d029040a5e3d428987faeeb7e0585db233
Kevin Galeazzi <53961230+kUs643@users.noreply.github.com>
2023-07-15 08:32:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,40 @@
 import streamlit as st
-import requests
-import csv
+import cv2
+import pytesseract
+import pandas as pd
+from PIL import Image
+import io
 import base64
 
-# OCR.space API key and endpoint
-api_key = 'K82787541488957'
-endpoint = 'https://api.ocr.space/parse/image'
+# 1. Drag and drop images
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 
-# Initialize an empty list to store image URLs
-image_urls = []
-
-# Start of Streamlit application
-st.title('OCR Application')
-
-uploaded_file = st.file_uploader("Choose an image file")
 if uploaded_file is not None:
-    image_urls.append(uploaded_file.getvalue())
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    
+    # 2. Optimize the image for OCR
+    # Convert the image to grayscale
+    grayscale = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+    
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(grayscale, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    
+    # Apply blur to smooth out the edges
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    
+    # 3. OCR to extract the information
+    # Remember to add your tesseract path here
+    pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract' 
+    result = pytesseract.image_to_string(img)
+    st.write(result)
+    
+    # 4. Store into a CSV
+    df = pd.DataFrame([result], columns=['Text'])
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as &lt;some_name&gt;.csv)'
+    st.markdown(href, unsafe_allow_html=True)
 
-# Button to start OCR process
-if st.button('Start OCR'):
-    # Loop through image URLs and perform OCR on each image
-    for url in image_urls:
-        # Request OCR using API endpoint and image URL
-        response = requests.post(endpoint, 
-            files={'url': (None, url)},
-            data={
-                'apikey': api_key,
-                'language': 'eng'
-            })
-
-        # Parse OCR results from response JSON
-        result = response.json()
-        if result['IsErroredOnProcessing']:
-            st.write(f'OCR error: {result["ErrorMessage"]}')
-        else:
-            parsed_results = result['ParsedResults']
-            if len(parsed_results) > 0:
-                text = parsed_results[0]['ParsedText']
-                st.write(f'OCR result for {url}: {text}')
-
-                # Write results to CSV
-                with open('ocr_results.csv', 'w', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow(["Image URL", "OCR Result"])
-                    writer.writerow([url, text])
-                    
-    # Download CSV button
-    if st.button('Download CSV'):
-        with open('ocr_results.csv', 'r') as file:
-            csv = file.read()
-        b64 = base64.b64encode(csv.encode()).decode()  # some strings
-        href = f'<a href="data:file/csv;base64,{b64}" download="ocr_results.csv">Download CSV File</a>'
-        st.markdown(href, unsafe_allow_html=True)
