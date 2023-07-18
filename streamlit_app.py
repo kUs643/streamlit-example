7d3d9b9f85f9be8913c94cfe4e6c94a1a2404905
Kevin Galeazzi <53961230+kUs643@users.noreply.github.com>
2023-07-18 17:33:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,64 @@
 import streamlit as st
+from PIL import Image
 import os
+import io
 
-# Streamlit configurations
-st.set_option('deprecation.showfileUploaderEncoding', False)
+# Coordenadas de corte para las diferentes partes
+PARTS = {
+    'part1': (55, 79, 55+197, 79+96),
+    'part2': (743, 72, 743+379, 72+91),
+    'part3': (332, 299, 332+1257, 299+298)
+}
 
-def save_image(image, image_name):
-    # Define the path where you want to save the images
-    image_path = './cache/'
+def cut_image(image, coords):
+    """Corta la imagen en las coordenadas dadas"""
+    return image.crop(coords)
 
-    # Check if the cache directory exists, if not, create it.
-    if not os.path.exists(image_path):
-        os.makedirs(image_path)
+def save_images(images, folder_name):
+    """Guarda las imágenes en la carpeta dada"""
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
 
-    # Save the image
-    with open(os.path.join(image_path, image_name), 'wb') as f:
-        f.write(image.getbuffer())
-    return f"Image {image_name} saved!"
+    for img_name, img in images.items():
+        img.save(os.path.join(folder_name, f"{img_name}.png"))
 
-for i in range(5):
-    st.write(f'### Game {i+1}')
+def handle_upload(file, part):
+    """Maneja la subida de archivos"""
+    if file is not None:
+        image = Image.open(file)
+        cut_images = {f"partida{part}-{part_name}": cut_image(image, coords) for part_name, coords in PARTS.items()}
+        return cut_images
+    return {}
 
-    image1 = st.file_uploader("Drop your first image here", type=['png', 'jpg', 'jpeg'], key=f'1-{i}')
-    image2 = st.file_uploader("Drop your second image here", type=['png', 'jpg', 'jpeg'], key=f'2-{i}')
+def app():
+    st.title("Analizador de imágenes de Valorant")
+    st.sidebar.title("Navegación")
+    page = st.sidebar.selectbox("Elige una página", ["Organizador", "Página 2", "Página 3"])
 
-    if image1 and image2:
-        image1_name = f'partida{i+1}-imagen1.png'
-        image2_name = f'partida{i+1}-imagen2.png'
+    if page == "Organizador":
+        st.header("Organizador de partidas")
 
-        save_message1 = save_image(image1, image1_name)
-        save_message2 = save_image(image2, image2_name)
+        all_images = {}
+        for i in range(1, 6):
+            st.subheader(f"Partida {i}")
+            uploaded_file = st.file_uploader(f"Sube la imagen para la partida {i}", type=['png', 'jpg', 'jpeg'])
+            images = handle_upload(uploaded_file, i)
+            all_images.update(images)
 
-        st.write(save_message1)
-        st.write(save_message2)
+            if i < 5:
+                st.markdown("---")  # Linea separadora
+
+        if st.button("Descargar imágenes"):
+            save_images(all_images, 'descargas')
+
+    elif page == "Página 2":
+        st.header("Página 2")
+        # Implementa la funcionalidad de la página 2 aquí
+
+    elif page == "Página 3":
+        st.header("Página 3")
+        # Implementa la funcionalidad de la página 3 aquí
+
+if __name__ == "__main__":
+    app()
+
