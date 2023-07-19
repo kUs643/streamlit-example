6a5cca9101a563742d2bc1d2190f69a62bb285cd
Kevin Galeazzi <53961230+kUs643@users.noreply.github.com>
2023-07-18 18:14:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageOps
+import numpy as np
+import cv2
+import requests
+import json
+import pandas as pd
 import os
 
 # Crear una lista para almacenar todas las imágenes de las partidas
@@ -46,6 +51,108 @@ if navigation == 'Organizador':
 
 elif navigation == 'Otro':
     st.write("Esta es la sección Otro.")
+    import streamlit as st
+from PIL import Image, ImageOps
+import numpy as np
+import cv2
+import requests
+import json
+import pandas as pd
+import os
+
+def process_image(img, part):
+    if part == 1:
+        # Convert the image to grayscale
+        img_gray = img.convert("L")
+
+        # Darken the image slightly
+        img_darkened = img_gray.point(lambda p: p * 0.9)
+
+        # Enhance contrast in the image
+        img_enhanced = img_darkened.point(lambda p: 255 * (p / 255) ** 0.8)
+
+        return img_enhanced
+
+    elif part == 2:
+        # Convert the image to grayscale
+        img_gray = img.convert("L")
+
+        # Binarize the image
+        threshold = 100  # You might need to adjust this value based on your specific image
+        img_binary = img_gray.point(lambda p: p > threshold and 255)
+
+        # Invert the image (if necessary)
+        img_inverted = Image.fromarray(np.array(img_binary) ^ 255)
+
+        # Ensure the image is in the correct mode for saving
+        final_img = img_inverted.convert("RGB")
+
+        return final_img
+
+    elif part == 3:
+        # Convert the image to grayscale
+        img_gray = img.convert('L')
+
+        # Convert the grayscale image to binary using Otsu's binarization
+        _, img_binary = cv2.threshold(np.array(img_gray), 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        # Convert the binary image back to PIL format
+        img_binary_pil = Image.fromarray(img_binary)
+
+        # Invert the colors of the binary image
+        img_inverted = ImageOps.invert(img_binary_pil)
+
+        # Define a structuring element for erosion
+        erosion_kernel = np.ones((3,3),np.uint8)
+
+        # Apply erosion to make the black regions thicker
+        img_eroded = cv2.erode(np.array(img_inverted), erosion_kernel, iterations = 1)
+
+        return img_eroded
+
+def ocr_space(image, api_key='Your_API_Key', language='eng'):
+    with open(image, 'rb') as image_file:
+        img_data = image_file.read()
+    url = 'https://api.ocr.space/parse/image'
+    headers = {'apikey': api_key}
+    data = {'language': language, 'isOverlayRequired': False, 'OCREngine': 2}
+    response = requests.post(url, headers=headers, data=data, files={'image': img_data}).json()
+
+    if response['IsErroredOnProcessing']:
+        st.error("The OCR.space API returned an error:")
+        st.write(response['ErrorMessage'])
+        return None
+
+    parsed_results = response['ParsedResults'][0]['ParsedText']
+    return parsed_results
+
+# Crear la barra de navegacion
+st.sidebar.title("Navegación")
+navigation = st.sidebar.radio("Ir a", ['Organizador', 'Extractor', 'Inyector'])
+
+if navigation == 'Extractor':
+    uploaded_files = st.file_uploader("Sube las imágenes", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
+
+    if st.button('Convertir'):
+        for uploaded_file in uploaded_files:
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+            img = Image.open(uploaded_file.name)
+            part = int(uploaded_file.name.split("-")[-1][0])  # Assumes the name is in the format 'partidaN-parteM.jpg'
+            img_processed = process_image(img, part)
+            img_processed.save(f'processed/{uploaded_file.name}')
+
+            result = ocr_space(f'processed/{uploaded_file.name}')
+
+            if result is not None:
+                st.write(f'Resultado para {uploaded_file.name}:')
+                st.write(result)
+
+        if st.button('Descargar resultados'):
+            # Assuming all the results have been stored in a pandas DataFrame called 'df'
+            df.to_csv('results.csv')
+            st.success('¡Resultados descargados!')
+
 
 elif navigation == 'Inyector':
     st.write("Esta es la sección Inyector.")
